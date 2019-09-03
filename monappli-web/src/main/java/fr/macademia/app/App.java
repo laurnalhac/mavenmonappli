1387fca5adb4549df64d40213932eb2dbcdfba50
laurnalhac <laurent.ginalhac@gmil.com>
2019-09-03 10:40:49 +0100

    first release

---
+++ monappli-web/src/main/java/fr/macademia/app/App.java
@@ -0,0 +1,14 @@
+package fr.macademia.app;
+
+/**
+ * Classe de gestion d'étudiants
+ * @author John Doe
+ * @version 2.6
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
